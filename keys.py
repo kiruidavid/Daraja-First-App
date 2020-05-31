4df5d003f76653103c47d67e0b35197442ca3385
unknown <david.wendot@gmail.com>
2020-05-31 09:28:44 +0300

    added to store vital information

---
+++ keys.py
@@ -0,0 +1,7 @@
+business_short_code = "174379" 
+phone_number = "254721949654"  
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "AbHBls0VVrs6oYaek6f8fwuSPok1mjEH"
+consumer_secret = "h2A6xCmGCJnAhl2N" 
+shortcode = "600610" 
+test_msisdn = "254708374149"
